e0fb45de6217e818306e8869dda0407d5c0a0347
aa-ag <aaron.aguerrevere@gmail.com>
2022-12-09 16:59:09 -0600

    Add CREATE first table query

--- psql/commands.sql
+++ psql/commands.sql
@@ -2,3 +2,8 @@
 -- psql postgres
 CREATE USER pg4e WITH PASSWORD 'thisisajoke';
 CREATE DATABASE people WITH OWNER 'pg4e';
+\dt
+CREATE TABLE users(
+    name VARCHAR(128),
+    email VARCHAR(128)
+);
