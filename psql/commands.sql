d80b425c68b0d53bf77c2ef011f04767f276314a
aa-ag <aaron.aguerrevere@gmail.com>
2022-12-13 20:22:26 -0600

    Add create track and track_raw commands

--- psql/commands.sql
+++ psql/commands.sql
@@ -31,3 +31,23 @@ CREATE TRIGGER set_timestamp
 BEFORE UPDATE ON keyvalue
 FOR EACH ROW
 EXECUTE PROCEDURE trigger_set_timestamp();
+
+CREATE TABLE album (
+  id SERIAL,
+  title VARCHAR(128) UNIQUE,
+  PRIMARY KEY(id)
+);
+
+CREATE TABLE track (
+    id SERIAL,
+    title VARCHAR(128),
+    len INTEGER, rating INTEGER, count INTEGER,
+    album_id INTEGER REFERENCES album(id) ON DELETE CASCADE,
+    UNIQUE(title, album_id),
+    PRIMARY KEY(id)
+);
+
+DROP TABLE IF EXISTS track_raw;
+CREATE TABLE track_raw
+ (title TEXT, artist TEXT, album TEXT, album_id INTEGER,
+  count INTEGER, rating INTEGER, len INTEGER);
