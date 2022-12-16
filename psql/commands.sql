420fecb7d7e6064b834e535be77ef49890ca4f72
aa-ag <aaron.aguerrevere@gmail.com>
2022-12-15 22:14:42 -0600

    Create tables for UNSECO exercise

--- psql/commands.sql
+++ psql/commands.sql
@@ -68,3 +68,32 @@ INSERT INTO album(title)
 SELECT album
 FROM track_raw
 ON CONFLICT DO NOTHING;
+
+-- UNESCO EXERCISE
+DROP TABLE unesco_raw;
+CREATE TABLE unesco_raw
+ (name TEXT, description TEXT, justification TEXT, year INTEGER,
+    longitude FLOAT, latitude FLOAT, area_hectares FLOAT,
+    category TEXT, category_id INTEGER, state TEXT, state_id INTEGER,
+    region TEXT, region_id INTEGER, iso TEXT, iso_id INTEGER);
+
+CREATE TABLE category (
+  id SERIAL,
+  name VARCHAR(128) UNIQUE,
+  PRIMARY KEY(id)
+);
+CREATE TABLE region (
+  id SERIAL,
+  name VARCHAR(128) UNIQUE,
+  PRIMARY KEY(id)
+);
+CREATE TABLE state (
+  id SERIAL,
+  name VARCHAR(128) UNIQUE,
+  PRIMARY KEY(id)
+);
+CREATE TABLE iso (
+  id SERIAL,
+  name VARCHAR(128) UNIQUE,
+  PRIMARY KEY(id)
+);
