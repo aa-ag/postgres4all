73d84e95b4e6c8e2c20857dbcc06b64c37a36fac
aa-ag <aaron.aguerrevere@gmail.com>
2022-12-18 19:58:20 -0600

    Add CREATE docs TABLE and populate it with three strings

--- psql/json.sql
+++ psql/json.sql
@@ -1,2 +1,14 @@
 select string_to_array('Hello world', ' ');
 select unnest(string_to_array('Hello world', ' '));
+
+CREATE TABLE docs (
+    id SERIAL,
+    doc TEXT,
+    PRIMARY KEY(id)
+);
+
+INSERT INTO docs (doc)
+VALUES ('This is SQL: a class focused on PostgreSQL'),
+('PostgreSQL is open source'),
+('PostgreSQL For Everybody is a Coursera Specialization');
+
