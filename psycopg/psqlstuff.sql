aeedfeec1c0695f4af799c0430b71d38ee0310fd
aa-ag <aaron.aguerrevere@gmail.com>
2023-03-11 08:52:38 -0600

    Add <-> logic to check places where the English word "tiny" comes before the word "tim"

--- psycopg/psqlstuff.sql
+++ psycopg/psqlstuff.sql
@@ -1,3 +1,5 @@
 CREATE INDEX pg19337_gin ON pg19337 USING gin(to_tsvector('english', body));
 
 SELECT id, body FROM pg19337 WHERE to_tsquery('english', 'love') @@ to_tsvector('english', body) LIMIT 10;
+
+SELECT id, body FROM pg19337 WHERE to_tsquery('english', 'tiny <-> tim') @@ to_tsvector('english', body) LIMIT 10;
