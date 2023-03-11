1bc2b8b6047c9379e4673907910e51864a415d7d
aa-ag <aaron.aguerrevere@gmail.com>
2023-03-11 08:30:23 -0600

    Add all inputs

--- psycopg/getinputs.sh
+++ psycopg/getinputs.sh
@@ -1,1 +1,4 @@
 wget http://www.gutenberg.org/cache/epub/19337/pg19337.txt
+wget https://www.pg4e.com/code/loadbook.py
+wget https://www.pg4e.com/code/myutils.py
+wget https://www.pg4e.com/code/hidden-dist.py
