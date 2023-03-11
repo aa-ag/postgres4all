f1829e1efa3d24da5af85ee37fa5450721cdae9a
aa-ag <aaron.aguerrevere@gmail.com>
2023-03-11 08:49:20 -0600

    Add query to find rows where body contains a specific english word

--- psycopg/psqlstuff.sql
+++ psycopg/psqlstuff.sql
@@ -1,1 +1,3 @@
 CREATE INDEX pg19337_gin ON pg19337 USING gin(to_tsvector('english', body));
+
+SELECT id, body FROM pg19337 WHERE to_tsquery('english', 'love') @@ to_tsvector('english', body) LIMIT 10;
