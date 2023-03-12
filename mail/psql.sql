57218b6c94c4c9e9b0eb4686f63d36a5d2d41c9d
aa-ag <aaron.aguerrevere@gmail.com>
2023-03-12 14:43:10 -0500

    Add query to search for 10 messages where the subject includes the word password

--- mail/psql.sql
+++ mail/psql.sql
@@ -1,3 +1,7 @@
 CREATE INDEX messages_gin ON messages USING gin(to_tsvector('english', body));
 SELECT to_tsvector('english', body) FROM messages LIMIT 1;
 SELECT id, to_tsquery('english', 'michigan') @@ to_tsvector('english', body) FROM messages;
+ALTER TABLE messages ADD COLUMN sender TEXT;
+SELECT SUBSTRING(headers, '\nFrom: [^\n]*<([^>]*)') FROM messages;
+UPDATE messages SET sender = SUBSTRING(headers, '\nFrom: [^\n]*<([^>]*)');
+SELECT subject, sender FROM messages WHERE to_tsquery('english', 'password') @@ to_tsvector('english', subject) LIMIT 10;
