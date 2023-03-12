7d1f33eeca12f4e54f367a2191a2da1c1850178c
aa-ag <aaron.aguerrevere@gmail.com>
2023-03-12 14:33:56 -0500

    Add query to check where the word 'michigan' is in the body of any messages

--- mail/psql.sql
+++ mail/psql.sql
@@ -1,2 +1,3 @@
 CREATE INDEX messages_gin ON messages USING gin(to_tsvector('english', body));
 SELECT to_tsvector('english', body) FROM messages LIMIT 1;
+SELECT id, to_tsquery('english', 'michigan') @@ to_tsvector('english', body) FROM messages;
