7cfbf8380be26e6921a3edf74f8ab75aac0cb608
aa-ag <aaron.aguerrevere@gmail.com>
2023-03-14 15:26:17 -0500

    Fix typo on .txt & table name

--- json/psql.sql
+++ json/psql.sql
@@ -1,4 +1,4 @@
 CREATE TABLE IF NOT EXISTS jtrack (id SERIAL, body JSONB);
-\copy jstrack (body) FROM 'library.txt' WITH CSV QUOTE E'\x01' DELIMITER '\x02';
-SELECT (body->>'count')::int FROM jstrack WHERE body @> '{"name":"Summer Nights"}';
-SELECT COUNT(*) FROM jstrack WHERE body ? 'favorite';
+\copy jtrack (body) FROM 'library.jstxt' WITH CSV QUOTE E'\x01' DELIMITER '\x02';
+SELECT (body->>'count')::int FROM jtrack WHERE body @> '{"name":"Summer Nights"}';
+SELECT COUNT(*) FROM jtrack WHERE body ? 'favorite';
