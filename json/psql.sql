72a875c4aedf4773c05159d3bd150655a612e95f
aa-ag <aaron.aguerrevere@gmail.com>
2023-03-14 15:33:29 -0500

    Fix \copy

--- json/psql.sql
+++ json/psql.sql
@@ -1,4 +1,4 @@
 CREATE TABLE IF NOT EXISTS jtrack (id SERIAL, body JSONB);
-\copy jtrack (body) FROM 'library.jstxt' WITH CSV QUOTE E'\x01' DELIMITER '\x02';
+\copy jtrack (body) FROM 'library.jstxt' WITH CSV QUOTE E'\x01' DELIMITER E'\x02';
 SELECT (body->>'count')::int FROM jtrack WHERE body @> '{"name":"Summer Nights"}';
 SELECT COUNT(*) FROM jtrack WHERE body ? 'favorite';
