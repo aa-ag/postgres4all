d53deeda95a1f8b119da98bcea70eac0082a9cce
aa-ag <aaron.aguerrevere@gmail.com>
2023-03-14 15:34:50 -0500

    Add pg_typeof SELECT

--- json/psql.sql
+++ json/psql.sql
@@ -1,4 +1,5 @@
 CREATE TABLE IF NOT EXISTS jtrack (id SERIAL, body JSONB);
 \copy jtrack (body) FROM 'library.jstxt' WITH CSV QUOTE E'\x01' DELIMITER E'\x02';
+SELECT pg_typeof(body) FROM jtrack LIMIT 1;
 SELECT (body->>'count')::int FROM jtrack WHERE body @> '{"name":"Summer Nights"}';
 SELECT COUNT(*) FROM jtrack WHERE body ? 'favorite';
