c0274478c56862cc683f41eb62309904c3c8b3e8
aa-ag <aaron.aguerrevere@gmail.com>
2023-03-13 20:03:12 -0500

    Add intersection logic

--- json/psql.sql
+++ json/psql.sql
@@ -1,2 +1,3 @@
 CREATE TABLE IF NOT EXISTS jtrack (id SERIAL, body JSONB);
 \copy jstrack (body) FROM 'library.txt' WITH CSV QUOTE E'\x01' DELIMITER '\x02';
+SELECT (body->>'count')::int FROM jstrack WHERE body @> '{"name":"Summer Nights"}';
