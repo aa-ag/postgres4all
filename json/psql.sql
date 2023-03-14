03bcd95b015247d6dc5a5308675d970f738313e1
aa-ag <aaron.aguerrevere@gmail.com>
2023-03-14 15:38:27 -0500

    Add example of aggregation from key

--- json/psql.sql
+++ json/psql.sql
@@ -2,5 +2,6 @@ CREATE TABLE IF NOT EXISTS jtrack (id SERIAL, body JSONB);
 \copy jtrack (body) FROM 'library.jstxt' WITH CSV QUOTE E'\x01' DELIMITER E'\x02';
 SELECT pg_typeof(body) FROM jtrack LIMIT 1;
 SELECT body->>'name' FROM jtrack LIMIT 3;
+SELECT MAX((body->>'count')::int) FROM jtrack;
 SELECT (body->>'count')::int FROM jtrack WHERE body @> '{"name":"Summer Nights"}';
 SELECT COUNT(*) FROM jtrack WHERE body ? 'favorite';
