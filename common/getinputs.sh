8e37325ab991391a8063e75693043465d4311ccc
aa-ag <aaron.aguerrevere@gmail.com>
2023-03-18 10:41:43 -0500

    Reorg files to have common elements in one folder

---
+++ common/getinputs.sh
@@ -0,0 +1,13 @@
+# wget http://www.gutenberg.org/cache/epub/19337/pg19337.txt
+# wget https://www.pg4e.com/code/loadbook.py
+# wget https://www.pg4e.com/code/myutils.py
+# wget https://www.pg4e.com/code/hidden-dist.py
+
+# http://mbox.dr-chuck.net/sakai.devel/1/2
+# wget https://www.pg4e.com/code/gmane.py
+# wget https://www.pg4e.com/code/datecompat.py
+# wget https://www.pg4e.com/code/hidden-dist.py
+
+# wget https://www.pg4e.com/code/library.jstxt
+wget https://www.pg4e.com/code/swapi.jstxt
+wget https://www.pg4e.com/code/myutils.jstxt
